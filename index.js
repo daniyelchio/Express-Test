eea21c1bdda327b7e702c49d961f6f98ce8c6f6d
Daniyel Cheah <cheahd@mcmaster.ca>
2022-05-23 18:46:13 -0400

    get *

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res) => {
+/* app.use((req, res) => {
     console.log("We got a new request");
     res.send('Request received');
+}) */
+
+app.get('/',(req, res) => {
+    console.log('Home requested');
+    res.send('Home Page');
+})
+
+app.get('/cats',(req, res) => {
+    console.log('Cats requested');
+    res.send('Meow');
+})
+
+app.get('/dogs',(req, res) => {
+    console.log('Dogs requested');
+    res.send('Woof');
+})
+
+app.get('*',(req, res) => {
+    console.log('Unknown Request');
+    res.send('Request is unknown');
 })
 
 app.listen(3000, () => {
